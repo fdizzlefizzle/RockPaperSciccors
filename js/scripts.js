a52431a738fbadd65e3e1491754c64e3a7d551e0
fordog <gamerfordy@gmail.com>
2021-08-19 22:15:39 +1000

    updated basic game logic to work with button entry. Start button temporarily acting as a reset. Scores count up but no overall winner announced yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,82 @@
 const choices = ["scissors", "paper", "rock"];
-let playerSelection
-let computerSelection
-let playerTotal 
-let computerTotal
-let turn
+const round = document.getElementById("round")
+const icons = document.getElementsByClassName("icon");
+const playerScore = document.getElementById("player-score")
+const computerScore = document.getElementById("computer-score")
+const roundResult = document.getElementById("round-result")
+const startGame = document.getElementById("start-game")
 
-function computerPlay(choices) {
+startGame.addEventListener("click", beginGame, false);
+startGame.setAttribute('draggable', false);
+
+function beginGame() {
+    resetGame()
+    playerScore.innerText = `${playerTotal}`;
+    computerScore.innerText = `${computerTotal}`; 
+    round.innerText = `${turn}`
+} 
+
+function resetGame() {
+    playerTotal = 0;
+    computerTotal = 0;
+    turn = 1;
+}
+
+function getPlayerSelection() {
+    playerSelection = this.getAttribute("id");
+    this.classList.add("selected")
+    this.addEventListener('transitionend', removeTransition => this.classList.remove("selected"))
+    playRound(playerSelection)
+}
+
+for (i = 0; i < icons.length; i++) {
+    icons[i].addEventListener('click', getPlayerSelection, false);
+    icons[i].setAttribute('draggable', false)
+}
+
+function getComputerChoice(choices) {
     let computerOutput = choices[Math.floor(Math.random()* choices.length)]
     return computerOutput;
 }
 
 function roundWin() {
-    return playerTotal = playerTotal + 1;
+    playerTotal = playerTotal + 1;
+    playerScore.innerText = playerTotal;
 }
 
 function roundLose() {
-    return computerTotal = computerTotal + 1;
+    computerTotal = computerTotal + 1;
+    computerScore.innerText = computerTotal;
 }
 
-function resetGame() {
-    playerTotal = 0;
-    computerTotal = 0;
-    turn = 0;
-}
 
-function playRound(playerSelection, computerSelection) {
+
+function playRound(playerSelection) {
+    computerSelection = getComputerChoice(choices)
     if (playerSelection === computerSelection) {
-        return "Tie Game";
+        roundResult.innerText = "The round is tied!"
     } else if (
         (playerSelection === "scissors" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "rock") ||
         (playerSelection === "rock" && computerSelection === "scissors")
     ) {
         roundWin();
-        return "Congratulations you win";
+        roundResult.innerText = `You WIN! ${playerSelection} beats ${computerSelection}`
     } else {
         roundLose();
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        roundResult.innerText = `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
+
 
 function game() {
     resetGame()
-    for (i = 0; i <5; i++) {
-        turn = turn + 1
-        console.log(`Turn ${turn}`)
-        
-        while (true) {
-            playerSelection = prompt("Scissors, Paper, or Rock?"); 
-            
-            if (playerSelection === null){ 
-                continue;                
-            } else if (
-                (playerSelection.toLowerCase() === "scissors") ||
-                (playerSelection.toLowerCase() === "paper") ||
-                (playerSelection.toLowerCase() === "rock")
-            ) {
-                playerSelection = playerSelection.toLowerCase()
-                break;
-            }
-        }
-
-        console.log(`Player chooses ${playerSelection}`)
-
-        computerSelection = computerPlay(choices)
-        console.log(`Computer chooses ${computerSelection}`)
-
-        playRound(playerSelection, computerSelection)
-        console.log(playRound(playerSelection, computerSelection))
-    }
+    turn = turn + 1
+    console.log(turn)
+    round.innerText = `${turn}`;
+    playRound(playerSelection);
     (playerTotal === computerTotal) ?
         (console.log(`After ${turn} rounds, the game has ended in a draw! YOU DUMB BITCH`)):
         (playerTotal > computerTotal) ?
-        (console.log(`After ${turn} rounds, YOU ARE THE WINNER YOU DUMB BITCH!!`)) : (console.log(`After ${turn} rounds, YOU FUCKING LOST YOU DUMB BITCH`))    
+        (console.log(`After ${turn} rounds, YOU ARE THE WINNER YOU DUMB BITCH!!`)) : (console.log(`After ${turn} rounds, YOU FUCKING LOST YOU DUMB BITCH`))
 }
-game()
